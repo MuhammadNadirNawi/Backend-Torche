0f746c8a8977786152eed40acb2020e4fe91f82c
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-02 23:39:19 +0800

    fisrt commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Backend server is running at http://localhost:${process.env.PORT}`);
+});
