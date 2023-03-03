4a4b6403a15e15321a5adb8daa3a8a1a146dace5
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-03 08:06:20 +0800

    import router and use

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connection = require("./config/database");
+const routes = require("./routers");
 
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
