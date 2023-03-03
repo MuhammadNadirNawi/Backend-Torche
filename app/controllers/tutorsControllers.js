c031ccfadd94eebef82fe8f01389408fb2322570
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-03 08:01:41 +0800

    add tutors controllers with CRUD

---
+++ app/controllers/tutorsControllers.js
@@ -0,0 +1,71 @@
+const Tutors = require("../models/tutors");
+
+const createTutors = async (req, res) => {
+  try {
+
+    const newTutor = new Tutors(req.body)
+    const tutor = await newTutor.save();
+    res.status(201).json({status: "success", message: "success create new mentor", data: {tutor},});
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      message: error.message,
+    })
+  }
+}
+
+const findAllTutors = async (req, res) => {
+  try {
+    const tutors = await Tutors.find();
+    if(tutors.length == 0) { 
+      return res.status(404).send({ message: "no tutors found", })
+    }
+    res.status(200).json({status: "success", message: "success get all tutors", data: tutors, })
+  } catch (error) {
+    return res.status(500).send({ message: error.message, })
+  }
+}
+
+const findTutorsById = async (req, res) => {
+  try {
+    const tutors = await Tutors.findById(req.params.id);
+    res.status(200).json({status: "success", message: "success get tutor", data: tutors, });
+  } catch (error) {
+    return res.status(500).send({ message: error.message, })
+  }
+};
+
+const updateTutorsById = async (req, res) => {
+  try {
+    const tutors = await Tutors.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    if(!tutors) {
+      return res.status(404).send({ message: "tutors not found", })
+    }
+    res.status(200).json({status: "success", message: "success update tutors",});
+  } catch (error) {
+    return res.status(500).send({ message: error.message, })
+  }
+};
+
+const deleteTutorsById = async (req, res) => {
+  try {
+    const tutors = await Tutors.findByIdAndDelete(req.params.id);
+    if(!tutors) {
+      return res.status(404).send({ message: "tutors not found", })
+    }
+    res.status(200).json({ status: "success", message: "tutors has been deleted."});
+  } catch (error) {
+    return res.status(500).send({ message: error.message, })
+  }
+};
+
+module.exports = {
+  createTutors,
+  findAllTutors,
+  findTutorsById,
+  updateTutorsById,
+  deleteTutorsById,
+}
