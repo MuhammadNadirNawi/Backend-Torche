ba14b949ada4b8910294482a619862299b9306d4
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-05 10:33:05 +0800

    create models verify account

---
+++ app/models/verify.js
@@ -0,0 +1,16 @@
+// const mongoose = require("mongoose");
+// const Users = require("./users");
+
+// const verifySchema = new mongoose.Schema(
+//   {
+//     idUser: { type:  mongoose.Types.ObjectId, ref: 'Users' },
+//     tokenVerify: { type: String, required: true,},
+//     expiredAt: {type: Date, required: true,},
+//   },
+//   { timestamps: true }
+//   );
+
+
+// const Verify = mongoose.model("Verify", verifySchema);
+
+// module.exports = Verify;
