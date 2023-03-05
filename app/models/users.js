a48bde491937543ef031343304c5951936dc44aa
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-05 10:32:36 +0800

    create models users

---
+++ app/models/users.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true,},
+    fullName: { type: String, required: true,},
+    password: {type: String, required: true,},
+    phone: {type: String, required: true,},
+    university: {type: String, required: true,},
+    department: {type: String, required: true,},
+    batch: { type: Number, required: true, },
+    knowFrom: { type: String, required: true, },
+    isEmailVerified: { type: Boolean, required: true, default: false },
+  },
+  { timestamps: true }
+  );
+
+const Users = mongoose.model("Users", userSchema);
+
+const verifySchema = new mongoose.Schema(
+  {
+    idUser: { type:  mongoose.Types.ObjectId, ref: 'Users' },
+    tokenVerify: { type: String, required: true,},
+    expiredAt: {type: Date, required: true,},
+  },
+  { timestamps: true }
+  );
+
+const Verify = mongoose.model("Verify", verifySchema);
+
+module.exports = { Users, Verify};
