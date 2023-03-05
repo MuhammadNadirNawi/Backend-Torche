158cf7ce4f5f0e061945957f4a4d23745563ef50
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-05 22:41:02 +0800

    using middleware

--- routers/tutors.js
+++ routers/tutors.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const tutorsControllers  = require("../app/controllers/tutorsControllers");
+const {verifyToken, authAdmin} = require("../middleware/auth");
 
 
-router.post('/api/tutors/create', tutorsControllers.createTutors);
+router.post('/api/tutors/create', verifyToken, authAdmin, tutorsControllers.createTutors);
 router.get('/api/tutors/findAll', tutorsControllers.findAllTutors);
 router.get('/api/tutors/:id', tutorsControllers.findTutorsById);
-router.put('/api/tutors/update/:id', tutorsControllers.updateTutorsById);
-router.delete('/api/tutors/delete/:id', tutorsControllers.deleteTutorsById);
+router.put('/api/tutors/update/:id', verifyToken, authAdmin, tutorsControllers.updateTutorsById);
+router.delete('/api/tutors/delete/:id', verifyToken, authAdmin, tutorsControllers.deleteTutorsById);
 
 module.exports = router
