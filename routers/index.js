0ca5cf5fc84969980a429aa030acf5024fc238a1
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-05 22:41:21 +0800

    using route users

--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
 const cors = require("cors")
+const cookieParser = require('cookie-parser')
+
 
 const tutors = require('./tutors')
 const auth = require('./auth')
+const users = require('./users')
 
 router.use(cors())
+router.use(cookieParser())
 router.use(tutors)
 router.use(auth)
+router.use(users)
 
 module.exports = router
