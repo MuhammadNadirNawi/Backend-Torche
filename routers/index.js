1a1bf36f626bc6e0ae96be685161a3640811ce3f
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-05 10:36:08 +0800

    add route auth

--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const cors = require("cors")
 
 const tutors = require('./tutors')
+const auth = require('./auth')
 
 router.use(cors())
 router.use(tutors)
+router.use(auth)
 
 module.exports = router
