76475b2260829bb5007d961481573c72004b13fa
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-06 10:59:58 +0800

    add validation if user not found

--- app/controllers/userControllers.js
+++ app/controllers/userControllers.js
@@ -15,6 +15,9 @@ const findAllUsers = async (req, res) => {
 const findUsersById = async (req, res) => {
   try {
     const user = await Users.findById(req.params.id);
+    if(!user) {
+      return res.status(404).send({ message: "users not found", })
+    }
     res.status(200).json({status: "success", message: "success get user", data: user, });
   } catch (error) {
     return res.status(500).send({ message: error.message, })
