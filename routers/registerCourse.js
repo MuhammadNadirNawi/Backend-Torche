00c6642713b765dc4acd09d2e00060a7f275df94
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-20 20:29:15 +0800

    fix: fix auth create register course

--- routers/registerCourse.js
+++ routers/registerCourse.js
@@ -3,7 +3,7 @@ const registerCourseControllers  = require("../app/controllers/registerCourseCon
 const {verifyToken, authUser, authAdmin} = require("../middleware/auth");
 
 
-router.post('/api/courses/register', verifyToken, authUser, registerCourseControllers.createRegisterCourses);
+router.post('/api/courses/register', verifyToken, registerCourseControllers.createRegisterCourses);
 router.get('/api/courses/register/findAll', verifyToken, authAdmin, registerCourseControllers.findAllRegisterCourses);
 router.get('/api/courses/register/:id',verifyToken, authAdmin, registerCourseControllers.findRegisterCourseById);
 router.delete('/api/courses/register/delete/:id', verifyToken, authAdmin, registerCourseControllers.deleteRegisterCourseById);
