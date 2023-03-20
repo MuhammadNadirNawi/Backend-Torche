99b64e4be048ae07f29f19e3b305d979abf0a73e
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-20 09:36:17 +0800

    add midleware auth

--- routers/courses.js
+++ routers/courses.js
@@ -3,10 +3,10 @@ const courseControllers  = require("../app/controllers/coursesControllers");
 const {verifyToken, authAdmin} = require("../middleware/auth");
 
 
-router.post('/api/courses/create', courseControllers.createCourses);
+router.post('/api/courses/create', verifyToken, authAdmin, courseControllers.createCourses);
 router.get('/api/courses/findAll', courseControllers.findAllCourses);
 router.get('/api/courses/:id', courseControllers.findCoursesById);
-router.put('/api/courses/update/:id', courseControllers.updateCoursesById);
-router.delete('/api/courses/delete/:id', courseControllers.deleteCoursesById);
+router.put('/api/courses/update/:id',verifyToken, authAdmin, courseControllers.updateCoursesById);
+router.delete('/api/courses/delete/:id', verifyToken, authAdmin, courseControllers.deleteCoursesById);
 
 module.exports = router
