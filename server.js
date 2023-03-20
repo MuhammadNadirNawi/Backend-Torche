18ada0e1846d1aa1f94e9616db6ff43f9891a2c5
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-20 09:28:53 +0800

    add: add middlware passport and cookies session

--- server.js
+++ server.js
@@ -4,16 +4,28 @@ const connectDatabase = require("./config/database");
 const routes = require("./routers");
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-
+const passport = require("passport");
+passportStrategy = require('./app/controllers/passport')
+const session = require('express-session')
 
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use(cookieParser())
 app.use(cors())  
+app.use(
+  session({
+		secret: "secr3t",
+    resave: false,
+    saveUninitialized: true,
+		cookie: { maxAge: 24 * 60 * 60 * 1000 } // 1 day
+})
+)
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(routes);
 
 connectDatabase();
 
