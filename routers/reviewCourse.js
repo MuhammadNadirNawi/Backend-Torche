90d0c1e7715c5db52804b6460a2beb628ef2a767
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-22 10:33:38 +0800

    update: change typo name validation

--- routers/reviewCourse.js
+++ routers/reviewCourse.js
@@ -4,7 +4,7 @@ const {verifyToken, authUser} = require("../middleware/auth");
 const {createCourseValidation, runValidation} = require("../validation/index");
 
 
-router.post('/api/reviews/courses/create', verifyToken, createCourseValidation, runValidation, reviewCoursesControllers.createReviewCourse);
+router.post('/api/reviews/courses/create', verifyToken, createReviewCourseValidation, runValidation, reviewCoursesControllers.createReviewCourse);
 router.get('/api/reviews/courses/findAll', reviewCoursesControllers.findAllReviewCourse);
 router.get('/api/reviews/courses/byUser', reviewCoursesControllers.findReviewCourseByUser);
 router.get('/api/reviews/courses/byCourse', reviewCoursesControllers.findReviewCourseByCourse);
