eb8971a88b1cbc2a1050bb104c526fa6f70cade2
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-03-28 13:11:43 +0800

    add router searching events

--- routers/events.js
+++ routers/events.js
@@ -7,6 +7,7 @@ const uploader = require("../middleware/uploader")
 
 router.post('/api/events/create', uploader.single('image'), eventsControllers.createEvents);
 router.get('/api/events/findAll', eventsControllers.findAllEvents);
+router.get('/api/events/search', eventsControllers.searchingEvents);
 router.get('/api/events/:id', eventsControllers.findEventsById);
 router.put('/api/events/update/:id', eventsControllers.updateEventsById);
 router.delete('/api/events/delete/:id', eventsControllers.deleteEventsById);
